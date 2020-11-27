ac60926097f32bd9691c57ba1bfaacd0215b94b7
Kcassie-WriteCode <3125921734@qq.com>
2020-11-27 14:37:48 +0800

    feat:修改

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
@@ -13,12 +12,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
