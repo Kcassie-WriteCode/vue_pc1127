11bc2d68baf7c95a056c14b1f4694aa7c807f1e6
Kcassie-WriteCode <3125921734@qq.com>
2020-12-02 11:47:26 +0800

    feat:search输入框完成

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -71,6 +71,13 @@ export default {
       this.searchText = "";
     });
   },
+  watch:{
+    $route(){
+      if (this.$route.path.indexOf("/search") === -1) {
+      this.searchText = "";
+    }
+    }
+  }
 };
 </script>
 
